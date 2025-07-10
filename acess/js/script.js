4212c4d92535bd13c233403dd937dcbf962ce0d0
mubeel <muhammadmdgh1@gmail.com>
2025-07-10 08:13:08 +0300

    making the app inter active

--- acess/js/script.js
+++ acess/js/script.js
@@ -1,1 +1,45 @@
-console.log("Script loaded successfully!");
+let todoList =
+    JSON.parse(localStorage.getItem('todoList')) || [
+        {text: 'Learn JavaScript'},
+        {text: 'Build a Todo App'}
+        
+    ]
+
+
+const todoListElement = document.getElementById('todo-list');
+const todoInput = document.getElementById('todo-input');
+
+let render = '';
+
+function renderTodoList() {
+    for(let i = 0; i < todoList.length; i++) {
+        console.log(todoList[i].text);
+        render += `
+            <li class="todo-item item${i}">
+                <span class="todo-text">${todoList[i].text}</span>
+                <button class="delete-button" onclick="deleteTodo(${i})">Delete</button>
+            </li>
+        `;
+        document.getElementById('todo-list').innerHTML = render;
+    }
+}
+function addTodo() {
+    const todotext = todoInput.value;
+    if(todotext.trim() === '') {
+        alert('Please enter a todo item');
+        return;
+    }else {
+        todoList.push({text: todotext});
+        todoInput.value = '';
+        render = '';
+        renderTodoList();
+        localStorage.setItem('todoList', JSON.stringify(todoList));
+    }
+}
+function deleteTodo(index) {
+    todoList.splice(index, 1);
+    render = '';
+    renderTodoList();
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+renderTodoList();
